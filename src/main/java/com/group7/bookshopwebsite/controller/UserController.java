903ecede8322cccd14e7483e6b098be2952a7fd4
L1uuuu <92938245+L1uuuu@users.noreply.github.com>
2023-08-04 10:36:02 +0700

    initial commit

---
+++ src/main/java/com/group7/bookshopwebsite/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.group7.bookshopwebsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("message", "Hello World");
+		return "admin/books";
+	}
+
+}
---
+++ src/test/java/com/group7/bookshopwebsite/BookshopwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.bookshopwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
